6ea3804d9972b2115ac6057415e939f938ff7db0
sinta73 <sintamahar220103@gmail.com>
2025-02-04 11:55:15 +0700

    Update Untitled-6.cpp

--- Untitled-6.cpp
+++ Untitled-6.cpp
@@ -1,38 +1,37 @@
-
 //sistem keanggotaan gym
 #include <iostream>
 using namespace std;
-
-int main(){
-    int jumlahLatihan;
-    string keanggotaan, silver, gold, platinum;
-
-    cout << "Kategori Anggota: ";
-    cin >> keanggotaan;
-    cout << "Latihan yang sudah berjalan: ";
-    cin >> jumlahLatihan;
-
-    if (keanggotaan == "silver"){
-        cout << "----------" << endl;
-        cout << "Silver" << endl;
-        cout << "Anda perlu latihan sebanyak " << 10 - jumlahLatihan << "X lagi bulan ini!" << endl;
-    }else{
-        if (keanggotaan == "gold"){
-            cout << "----------" << endl;
-            cout << "Gold" << endl;;
-            cout << "Anda perlu latihan sebanyak " << 20 - jumlahLatihan << "X lagi bulan ini!" << endl; 
-    }else{
-        if (keanggotaan == "platinum"){
-        cout << "----------" << endl;
-        cout << "Platinum" << endl;
-        cout << "Silahkan latihan sesuka hati anda!" << endl;
-    }else{
-        cout << "tidak valid";
-    }    
+int main() {
     
-    } 
+    string jenisKeanggotaan;
+    int jumlahLatihan, batasLatihan;
+    
+    cout << "Masukkan Jenis Keanggotaan ( silver/gold/platinum ) : ";
+    cin >> jenisKeanggotaan;
+    
+    if ( jenisKeanggotaan == "silver" ) {
+        batasLatihan = 10;
+    } else { 
+         if ( jenisKeanggotaan == "gold" ) {
+            batasLatihan = 20;
+    } else {
+         if ( jenisKeanggotaan == "platinum" ) {
+            cout << "Anda Tidak memiliki batas\n";
+            return 0;
+            } else {
+                cout << "Tidak valid!\n";
+            } return 0;
+        }
     }
-
+     
+   cout << "Masukkan jumlah latihan yang dilakukan : ";
+   cin >> jumlahLatihan;
+   
+   if ( jumlahLatihan < batasLatihan) {
+       cout << "Anda masih bisa latihan sebanyak " << (batasLatihan - jumlahLatihan) << " kali lagi bulan ini\n";
+   } else {
+       cout << "Anda sudah mencapai batas latihan bulan ini\n";
+   }
     return 0;
 }
     
